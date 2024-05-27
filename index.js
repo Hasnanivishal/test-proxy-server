7ea7695d56d8fc5150bc858152d2b634b3a8f67b
Vishal Hasnani <hasnanivishal@gmail.com>
2024-05-27 13:52:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const axios = require('axios');
+const HttpsProxyAgent = require("https-proxy-agent");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Define a route that makes an Axios request
 app.get('/fetch-data', async (req, res) => {
+
+  const httpsAgent = new HttpsProxyAgent.HttpsProxyAgent('https://node-proxy-server.onrender.com');
+  
   try {
     const response = await axios.get('https://api.github.com/search/users?q=hasnanivishal&per_page=10&page=1', {
-      proxy: {
-          protocol: 'https',
-          host: 'node-proxy-server.onrender.com',
-          port: 443,
-        },
-      httpsAgent: new (require('https')).Agent({ rejectUnauthorized: false })
+      // proxy: {
+      //   protocol: 'https',
+      //   host: 'node-proxy-server.onrender.com',
+      //   port: 443,
+      // },  
+      httpsAgent: httpsAgent
+      // httpsAgent: new (require('https')).Agent({ rejectUnauthorized: false })
     });
     res.json(response.data);
   } catch (error) {
